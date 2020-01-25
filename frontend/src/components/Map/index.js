2d15fba30db96d355622cb3a485b992fcfc869b8
Rubens Batista <batistarubinho@gmail.com>
2020-01-25 18:30:17 -0300

    Adicionado os marcadores de devs no mapa

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
-import MapGL from "react-map-gl";
+import MapGL, { Marker } from "react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./styles.css";
 
 const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 const MAX_ZOOM = 18;
 const MIN_ZOOM = 5;
+
+const devs = [
+  {
+    _id: 2,
+    location: {
+      coordinates: [-46.801004000000006, -23.649491899999997]
+    },
+    avatar_url: "https://avatars1.githubusercontent.com/u/2254731?s=400&v=4",
+    name: "Diego Fernandes",
+    github_username: "diego3g",
+    bio: "Alguma coisa legal",
+    techs: ["ReactJS", "NodeJS"]
+  }
+];
 
 const Map = ({ width, height }) => {
   const [viewport, setViewport] = useState({
@@ -25,7 +40,35 @@ const Map = ({ width, height }) => {
       mapStyle="mapbox://styles/mapbox/outdoors-v11"
       onViewportChange={viewport => setViewport({ ...viewport })}
       onResize={() => setViewport({ ...viewport, width, height })}
-    ></MapGL>
+    >
+      {devs.map(dev => (
+        <Marker
+          key={dev._id}
+          longitude={dev.location.coordinates[0]}
+          latitude={dev.location.coordinates[1]}
+          className="dev-marker"
+        >
+          <img
+            onClick={e => e.target.parentNode.classList.toggle("-show")}
+            className="avatar"
+            src={dev.avatar_url}
+            alt={`Imagem de ${dev.name}`}
+          />
+
+          <div className="dev-infos">
+            <a
+              href={`https://github.com/${dev.github_username}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="name">{dev.name}</p>
+              <p className="bio">{dev.bio}</p>
+              <p className="techs">{dev.techs.join(", ")}</p>
+            </a>
+          </div>
+        </Marker>
+      ))}
+    </MapGL>
   );
 };
 
